084eb99ebc38ef460522367337d200cb04264df3
Guillaume Leclerc <guill@sleeplessmind.com.mo>
2026-02-11 19:20:29 +0800

    add pasting "" on selected text after 3 seconds

--- PagesNumbersKeynoteBugWhenPastingEmptyPasteboard/ContentView.swift
+++ PagesNumbersKeynoteBugWhenPastingEmptyPasteboard/ContentView.swift
@@ -10,10 +10,27 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Button("paste \"\" on selected text") {
+                sleep(3)
+                                
+                NSPasteboard.general.clearContents()
+                NSPasteboard.general.setString("", forType: .string)
+                
+                guard
+                    let downEvent =  CGEvent(keyboardEventSource: CGEventSource(stateID: .privateState), virtualKey: 9, keyDown: true),
+                    let upEvent =  CGEvent(keyboardEventSource: CGEventSource(stateID: .privateState), virtualKey: 9, keyDown: false)
+                else {
+                    return
+                }
+                
+                downEvent.flags = []
+                downEvent.flags.insert(.maskCommand)
+                upEvent.flags = []
+                upEvent.flags.insert(.maskCommand)
+                
+                downEvent.post(tap: .cghidEventTap)
+                upEvent.post(tap: .cghidEventTap)
+            }
         }
         .padding()
     }
