59babb2f6c7642136decbc96aedc2e9f5d6fac4b
Guillaume Leclerc <guill@sleeplessmind.com.mo>
2026-02-11 19:10:36 +0800

    create Xcode proj

---
+++ PagesNumbersKeynoteBugWhenPastingEmptyPasteboard/PagesNumbersKeynoteBugWhenPastingEmptyPasteboardApp.swift
@@ -0,0 +1,17 @@
+//
+//  PagesNumbersKeynoteBugWhenPastingEmptyPasteboardApp.swift
+//  PagesNumbersKeynoteBugWhenPastingEmptyPasteboard
+//
+//  Created by Guillaume Leclerc on 11/02/2026.
+//
+
+import SwiftUI
+
+@main
+struct PagesNumbersKeynoteBugWhenPastingEmptyPasteboardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
